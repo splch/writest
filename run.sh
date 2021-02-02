2edaa40129d396b3983e59ec16005ea0cdee237e
splch <spencer.l.churchill@gmail.com>
2021-02-02 13:25:47 -0800

    exclude license from chrome extension

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-zip -r -9 --exclude=*.git* --exclude=*.DS_Store* --exclude=*.vscode* --exclude=*.sh* --exclude=sources.txt ../writest.zip *
+zip -r -9 --exclude=*.git* --exclude=*.DS_Store* --exclude=*.vscode* --exclude=*.sh* --exclude=sources.txt --exclude=LICENSE ../writest.zip *
